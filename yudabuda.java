fe77b59730064c7e92692f01360fbb93e33196ef
TapChap <shaigimel@gmail.com>
2025-05-15 18:06:56 +0300

    create yudabuda class

---
+++ yudabuda.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class yudabuda {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter your name: ");
+        String name = in.nextLine();
+
+        System.out.println("Hello " + name + "!");
+    }
+}
